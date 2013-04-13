20d2ad352e8ce0fc59e70026727ef3cd61f7769e
Stuart P. Bentley <stuart@testtrack4.com>
2013-04-13 01:18:21 -0400

    Simpler

--- userscript.js
+++ userscript.js
@@ -3,9 +3,8 @@
 // @description Adds a green style rule for links that open in a new tab.
 // @include     *
 // @grant none
-// @version 1.1
+// @version 1.2
 // ==/UserScript==
 
-var s=document.createElement('style');
-s.appendChild(document.createTextNode('a[target="_blank"] {color: green}'));
-document.getElementsByTagName('head')[0].appendChild(s);
+document.head.appendChild(document.createElement('style'))
+  .textContent='a[target="_blank"] {color: green}';
